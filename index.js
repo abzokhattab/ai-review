ca919809c9b904d57776bac5c479e7e68356798c
abzokhattab <abdelrahman@energenious>
2023-03-21 15:58:03 +0200

    Tweeking the prompt message

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ async function main() {
     process.exit(1);
   }
 
-  let prompt = `I want you to act like a code reviewer. I'll give you the output of the "git diff" command as an input, and your job is to review the code changes and make suggestions in case you have any. Return a complete sentence without repeating yourself: ${diff}`;
+  let prompt = `I want you to act like a code reviewer. I'll give you the output of the "git diff" command as an input, and your job is to review the code changes, spot any bad coding practices and make suggestions in case you have any. Return a complete sentence without repeating yourself: ${diff}`;
 
   console.log(chalk.gray("Generating your AI code review...\n"));
   const aiCodeReview = await generateCodeReview(prompt);
